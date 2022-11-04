ff6c7400a5ea27c002d623048e26a7923d83f09c
rahmanazizf <100136072+rahmanazizf@users.noreply.github.com>
2022-11-04 08:27:22 +0700

    Add files via upload

---
+++ regexp/remove-whitespace-from-start-and-end.js
@@ -0,0 +1,13 @@
+/*
+Remove Whitespace from Start and End
+Sometimes whitespace characters around strings are not wanted but are there. Typical processing of strings is to remove the whitespace at the start and end of it.
+
+Write a regex and use the appropriate string methods to remove whitespace at the beginning and end of strings.
+
+Note: The String.prototype.trim() method would work here, but you'll need to complete this challenge using regular expressions.
+*/
+
+let hello = "   Hello, World!  ";
+let wsRegex = /(\S+)/g; // Change this line
+let result = hello.match(wsRegex).join(" "); // Change this line
+console.log(result);
